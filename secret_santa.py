283424c3d8f8ac9cae252d4514855b52137b294f
Pranita Khandelwal <pranita.khandelwal@gmail.com>
2020-12-25 22:20:31 +0530

    Update secret_santa.py
    added functions to create new game

--- secret_santa.py
+++ secret_santa.py
@@ -7,9 +7,9 @@ def generate_secretIds(names):
     df = pd.DataFrame({'name': names})
     secretIds = random.sample(range(100000, 1000000), len(df))
     df['secretId'] = secretIds
-    print(df)
     df = df.reset_index()
     df.to_csv("santa_data.csv", index=False)
+    st.success('SecretId data dumped. ')
 
 
 def assign_santas():
@@ -37,12 +37,24 @@ def find_giftee(secretId):
         st.markdown(f"### {df.at[giftee, 'name']}!")
         st.balloons()
 
+if __name__ == '__main__':
 
-st.title("ADDA Secret Santa 2020")
+    st.title("Secret Santa Generator")
 
-if st.button("Assign santa-giftee pairs"):
-    df = assign_santas()
+    st.header("Start new game")
+    input_names = st.text_area("Enter names of people participating separated by comma")
 
-secretId = st.number_input("Enter your secretId:")
-if st.button("Find my giftee!"):
-    find_giftee(secretId)
+    if st.button("Generate Secret Ids"):
+        names = list(map(lambda x: x.strip(), input_names.split(", ")))
+        if len(names) < 2:
+            st.warning("Add atleast two names to play game. ")
+        generate_secretIds(names)
+
+    
+    if st.button("Randomly assign santa-giftee pairs"):
+        df = assign_santas()
+
+    st.header("Find giftee")
+    secretId = st.number_input("Enter your secretId:")
+    if st.button("Find my giftee!"):
+        find_giftee(secretId)
